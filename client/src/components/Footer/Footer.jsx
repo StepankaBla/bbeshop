4bc330b54d16af8d81b8b680bced1969ad4549b1
StepankaBla <92729296+StepankaBla@users.noreply.github.com>
2023-01-15 22:26:02 +0100

    responsive navbar

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,50 +4,7 @@ import "./Footer.scss";
 export const Footer = () => {
   return (
     <div className="footer">
-      <div className="top">
-        <div className="item">
-          <h1>Categories</h1>
-          <span>Painting</span>
-          <span>Jewelry</span>
-        </div>
-        <div className="item">
-          <h1>Links</h1>
-          <span>Cookies</span>
-        </div>
-        <div className="item">
-          <h1>About</h1>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-            obcaecati repudiandae laudantium labore explicabo. Dignissimos
-            quaerat aspernatur magni corporis. Dolore eligendi magnam facilis
-            voluptatem similique accusantium quod, a dolorum odio?
-          </span>
-        </div>
-        <div className="item">
-          <h1>Contact</h1>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            quos atque possimus nulla. Corporis laboriosam quaerat quas adipisci
-            voluptatum inventore alias dicta tenetur ex sapiente quasi, aut
-            itaque natus exercitationem.
-          </span>
-        </div>
-      </div>
-      <div className="contact-form">
-        <form id="contact-form" method="post" action="contact-form-handler.php">
-        <input name="name" type="name" class="form-control" placeholder="Your Name"
-           required/><br></br>
-          <input name="email" type="email" class="form-control" placeholder="Your Email"
-           required/><br></br>
-           <textarea name="message" class="form-control" placeholder="Message" row="4"
-           required/><br></br>
-           <input type="submit" class="form-control submit" value="SEND MESSAGE"/>
-        </form>
-      </div>
-      <div className="bottom">
-        <span className="logo">BBeshop</span>
-        <span className="copyright">© Copyright 2023. All Rights Reserved</span>
-      </div>
+       <img src="/img/1.jpg" className="pic" alt="" />
     </div>
   );
 };
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useRef } from "react";
+import { FaBars, FaSearch, FaShopify, FaStore, FaTimes } from "react-icons/fa";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonIcon from "@mui/icons-material/Person";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -6,54 +8,52 @@ import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 export const Navbar = () => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
-    <div className="navbar">
-      <div className="wrapper">
-        <div className="left">
-          <div className="item">
-            <Link className="link nav" to="/products/1">
-              painting
-            </Link>
-          </div>
-          <div className="item">
-            <Link className="link nav" to="/products/2">
-              jewelry
-            </Link>
-          </div>
-          <div className="item">
-            <Link className="link nav" to="/">
-              home
-            </Link>
-          </div>
-        </div>
-        <div className="center">
-          <Link className="link logo" to="/">
-            <img src="/img/logo.png" alt="" height="100" />
+    <>
+      <div className="logo-container">
+        <img src="/img/logo2.png" alt="" height="100" />
+      </div>
+      <div className="icons-container"></div>
+      <header>
+        <div className="icon-container">
+          <Link className=" icon" to="/">
+            <FaStore />
+          </Link>
+          <Link className=" icon" to="/">
+            <FaSearch />
           </Link>
         </div>
-        <div className="right">
-          
-          <div className="item">
-            <Link className="link nav" to="/">
-              about
-            </Link>
-          </div>
-          <div className="item">
-            <Link className="link nav" to="/">
-              contact
-            </Link>
-          </div>
-          <div className="icons">
-            <div className="cartIcon">
-              <div className="icon"><SearchIcon /></div>
-              <div className="icon"><PersonIcon /></div>
-              <div className="icon"><ShoppingCartIcon /></div>
-              <span>0</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        <nav ref={navRef}>
+          <Link className="link" to="/products/1">
+            painting
+          </Link>
+          <Link className="link" to="/products/2">
+            jewelry
+          </Link>
+          <Link className="link" to="/">
+            home
+          </Link>
+          <Link className="link" to="/">
+            about
+          </Link>
+          <Link className="link" to="/">
+            contact
+          </Link>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </header>
+    </>
   );
 };
 
